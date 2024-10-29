7c8c2ec9141ef48a85a8218bc59e6930dbffbb60
Mosiah Andrade <54685972+Mosiah-A@users.noreply.github.com>
2024-10-29 18:43:44 +0000

    cread projects

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,32 @@
-const projectImages = document.querySelectorAll('img');
-const projectModal = document.getElementById('projectModal');
-const closeModalBtn = document.getElementById('closeModal');
 
-projectImages.forEach((img) => {
-    img.addEventListener('click', () => {
-        projectModal.showModal();
+projectjson = {
+    "linkBio": {
+        "nome": "Link na Bio",
+        "descrição": "Uma Pagina de link na bio para ser usada em um perfio do Instagram",
+        "imagempath": "images/pojetos-thumbmail/linkBio.png",
+        "Tecnologias": ["HTML", "CSS"]
+    }
+}
+
+const projectImgs = document.querySelectorAll(".linkBio");
+const closeModalBtn = document.getElementById("closeModal");
+const modal = document.getElementById("projectModal");
+const content = document.getElementById("content");
+
+projectImgs.forEach(linkbio => {
+    linkbio.addEventListener('click', () => {
+        const element = projectjson["linkBio"];
+        content.innerHTML = `
+            <button id="closeModal">Fechar</button>
+            <h3>${element["nome"]}</h3>
+            <p>${element["descrição"]}</p>
+            <img src="${element["imagempath"]}" alt="${element["nome"]} " width="450">
+        `;
+        modal.showModal();
+
+        // Adiciona listener ao botão "Fechar" dentro do modal
+        document.getElementById("closeModal").addEventListener('click', () => {
+            modal.close();
+        });
     });
-});
-
-closeModalBtn.addEventListener('click', () => {
-    projectModal.close();
 });
