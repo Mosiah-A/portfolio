0597173a756699f2b3963f43fc9fa28ccb1129f4
Mosiah Andrade <54685972+Mosiah-A@users.noreply.github.com>
2024-10-30 18:50:23 +0000

    add modal to others projects and add style

--- scripts/test.js
+++ scripts/test.js
@@ -1,32 +1,73 @@
 
-projectjson = {
+const projectjson = {
     "linkBio": {
         "nome": "Link na Bio",
         "descrição": "Uma Pagina de link na bio para ser usada em um perfio do Instagram",
         "imagempath": "images/pojetos-thumbmail/linkBio.png",
-        "Tecnologias": ["HTML", "CSS"]
+        "Tecnologias": ["HTML", "CSS"],
+        "link": "https://mosiah-a.github.io/byu/"
+    },
+    "avengers": {
+        "nome": "Pagina de login",
+        "descrição": "Uma pagina de Login com o Tema dos Vingadores",
+        "imagempath": "images/pojetos-thumbmail/avengers.png",
+        "Tecnologias": ["HTML", "CSS"],
+        "link": "https://mosiah-a.github.io/portfolio/projetos/login/index.html"
+    },
+    "veterinaria": {
+        "nome": "Pagina Institucional de Veterinaria",
+        "descrição": "Uma pagina de uma clinica veterinaria ficticia",
+        "imagempath": "images/pojetos-thumbmail/veterinaria.png",
+        "Tecnologias": ["HTML", "CSS"],
+        "link": "https://mosiah-a.github.io/portfolio/projetos/veterinariaHomePage/"
     }
-}
+};
 
-const projectImgs = document.querySelectorAll(".linkBio");
+
+const linkBio = document.querySelectorAll(".linkBio");
+const avengers = document.querySelectorAll(".avengers");
+const veterinaria = document.querySelectorAll(".veterinaria");
 const closeModalBtn = document.getElementById("closeModal");
 const modal = document.getElementById("projectModal");
 const content = document.getElementById("content");
 
-projectImgs.forEach(linkbio => {
-    linkbio.addEventListener('click', () => {
-        const element = projectjson["linkBio"];
-        content.innerHTML = `
-            <button id="closeModal">Fechar</button>
-            <h3>${element["nome"]}</h3>
-            <p>${element["descrição"]}</p>
-            <img src="${element["imagempath"]}" alt="${element["nome"]} " width="450">
-        `;
-        modal.showModal();
+function createmodal(project) {
+    const element = projectjson[project];
+    const technologies = element.Tecnologias.map(tech => `<li>${tech}</li>`).join('');
+    content.innerHTML = `
+        <button id="closeModal">Fechar</button>
+        <h3>${element["nome"]}</h3>
+        <p>${element["descrição"]}</p>
+        <img src="${element["imagempath"]}" alt="${element["nome"]} " width="450" loading="lazy">
+        <h4>Tecnologias</h4>
+        <ul>
+            ${technologies}
+        </ul>
+        <a href="${element.link}" target="_blank" rel="noopener noreferrer">Acesse o Site</a>
+    `;
+    modal.showModal();
 
         // Adiciona listener ao botão "Fechar" dentro do modal
         document.getElementById("closeModal").addEventListener('click', () => {
             modal.close();
         });
+}
+linkBio.forEach(linkbio => {
+    linkbio.addEventListener('click', () => {
+       createmodal("linkBio")
+        
+    });
+});
+
+avengers.forEach(Avangers => {
+    Avangers.addEventListener('click', () => {
+       createmodal("avengers")
+        
+    });
+});
+veterinaria.forEach(project => {
+    project.addEventListener('click', () => {
+       createmodal("veterinaria")
+        
     });
 });
